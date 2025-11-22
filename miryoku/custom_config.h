ad060b8b585871b80deee6c1e5cbf722b7fbd420
Stalker Humanoid <stalker@fortress.city>
2025-11-22 14:49:04 -0500

    try this

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+&kp B,             &kp L,             &kp D,             &kp W,             &kp Z,                &kp SQT,           &kp F,             &kp O,             &kp U,             &kp J, \
+U_MT(LGUI, N),     U_MT(LALT, R),     U_MT(LCTRL, T),    U_LT(U_MOUSE, S),  &kp G,                &kp Y,             U_MT(DEL, H),    U_MT(LCTRL, A),    U_MT(LALT, E),     U_MT(LGUI, I), \
+U_LT(U_BUTTON, Q), U_MT(RALT, X),     &kp M,             &kp C,             &kp V,                &kp K,             &kp P,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH), \
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),&sk LSHFT,          U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, TAB),  U_NP,              U_NP
